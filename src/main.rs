2e0c5dc8116d194e37b76850e203ec70d007cebf
Josiah Hunsinger <fencepost13302@gmail.com>
2022-05-31 21:49:26 +0700

    Passing on all sample inputs but submission is failing the time limit on one of the tests

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,40 @@ impl Node {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let edges  = parse_input_into_edges(io::BufReader::new(io::stdin()));
+    let nodes  = nodes_from_edges(&edges);
+    let answer = get_puzzle_answer(&nodes);
+    println!("{}", answer);
 }
 
 fn get_puzzle_answer(nodes: &Vec<Node>) -> usize {
-    0
+    nodes.iter()
+        .enumerate() 
+        .filter(|(_, node)| node.next_nodes.len() == 0)
+        .map(|(i, _)| max_trimmed_depth(nodes, i))
+        .max()
+        .unwrap()
+}
+
+fn max_trimmed_depth(nodes: &Vec<Node>, n: usize) -> usize {
+    let this_node = &nodes[n];
+    
+    if this_node.prev_nodes.len() <= 1 {
+        return 1;
+    }
+
+    this_node.prev_nodes.iter()
+        .map(|&i| (i, &nodes[i]))
+        .filter(|(_, prev_node)| prev_node.next_nodes.len() > 1)
+        .map(|(i, _)| max_trimmed_depth(&nodes, i))
+        .max()
+        .unwrap() + 1
 }
 
 fn nodes_from_edges(edges: &Vec<Vec<usize>>) -> Vec<Node> {
     let &max_node_ind = edges.iter()
         .flatten()
+        .chain([&0])
         .max()
         .unwrap();
     
@@ -38,7 +62,7 @@ fn nodes_from_edges(edges: &Vec<Vec<usize>>) -> Vec<Node> {
     nodes
 }
 
-fn parse_puzzle_input<R>(input: R) -> Vec<Vec<usize>>
+fn parse_input_into_edges<R>(input: R) -> Vec<Vec<usize>>
     where R: io::BufRead
 {
         input.lines()
@@ -48,12 +72,27 @@ fn parse_puzzle_input<R>(input: R) -> Vec<Vec<usize>>
             .collect()
 }
 
+
 #[test]
-fn test_parse_input() {
-    let mut input_stream = io::Cursor::new(include_str!("../test_input_2.txt"));
-    let parsed_output = parse_puzzle_input(&mut input_stream);
-    assert_eq!(parsed_output, vec![]);
+fn test_parse_input_1() {
+    let mut input_stream = io::Cursor::new(include_str!("../test_input_1.txt"));
+    let parsed_edges = parse_input_into_edges(&mut input_stream);
+    let correct_edges = vec![
+        vec![1,2],
+        vec![2,3],
+        vec![1,3]
+    ];
+    assert_eq!(parsed_edges, correct_edges);
 }
+
+#[test]
+fn test_parse_input_2() {
+    let mut input_stream = io::Cursor::new(include_str!("../test_input_2.txt"));
+    let parsed_edges = parse_input_into_edges(&mut input_stream);
+    let correct_edges: Vec<Vec<usize>> = Vec::new();
+    assert_eq!(parsed_edges, correct_edges);
+}
+
 
 #[test]
 fn test_nodes_from_edges() {
@@ -75,4 +114,28 @@ fn test_nodes_from_edges() {
     
     assert_eq!(nodes[3].next_nodes, vec![]);
     assert_eq!(nodes[3].prev_nodes, vec![0]);
+}
+
+#[test]
+fn test_puzzle_solution_1() {
+    let mut input_stream = io::Cursor::new(include_str!("../test_input_1.txt"));
+    let edges = parse_input_into_edges(&mut input_stream);
+    let nodes = nodes_from_edges(&edges);
+    assert_eq!(get_puzzle_answer(&nodes), 2);
+}
+
+#[test]
+fn test_puzzle_solution_2() {
+    let mut input_stream = io::Cursor::new(include_str!("../test_input_2.txt"));
+    let edges = parse_input_into_edges(&mut input_stream);
+    let nodes = nodes_from_edges(&edges);
+    assert_eq!(get_puzzle_answer(&nodes), 1);
+}
+
+#[test]
+fn test_puzzle_solution_3() {
+    let mut input_stream = io::Cursor::new(include_str!("../test_input_3.txt"));
+    let edges = parse_input_into_edges(&mut input_stream);
+    let nodes = nodes_from_edges(&edges);
+    assert_eq!(get_puzzle_answer(&nodes), 3);
 }
